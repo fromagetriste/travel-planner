e95061ca4d573307320dfadb603dc04a16d3d9ae
fromagetriste __ <fromagetriste@gmail.com>
2025-08-07 12:25:33 -0600

    changed Globe UI

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -84,8 +84,7 @@ export default function GlobePage() {
                   {isLoading ? (
                     <div className="flex items-center justify-center h-full">
                       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 "></div>
-                      <br />
-                      <span>Up to 30 seconds ...</span>
+                      <div>Up to 30 seconds ...</div>
                     </div>
                   ) : (
                     <div className="flex items-center justify-center relative w-full h-[400px] sm:h-[600px]">
